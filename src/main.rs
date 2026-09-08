c00bfa526255a811fe1269f93dbecc675c27bf1f
daveclinton <clintondavid46@gmail.com>
2026-09-08 15:09:31 +0300

    feat: setup docker to be up and running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+    tracing_subscriber::fmt::init();
+
+    let state = AppState::init().await;
+    sqlx::migrate!().run(&state.db).await.unwrap();
+
+    let app = Router::new()
+        .nest("/auth", routes::auth::router())
+        .nest("/payments", routes::payments::router())
+        .nest("/ai", routes::ai::router())
+        .layer(TraceLayer::new_for_http())
+        .layer(CorsLayer::permissive())
+        .with_state(state);
+    
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:7070").await.unwrap();
+        axum::serve(listener, app).await.unwrap();
 }
--- src/state.rs
+++ src/state.rs
@@ -3,5 +3,5 @@ pub struct AppState(
     pub db: PgPool,
     pub redis: redis::Client,
     pub jwt_secret: String,
-    pub ai_client: async_openai::Client<async_openai::config::OpenAIConfig>
+    pub ai_client: async_openai::Client<async_openai::config::OpenAIConfig>,
 )
