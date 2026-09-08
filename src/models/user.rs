59892e07461fb857eb75c43852efaf6daebb27c4
daveclinton <clintondavid46@gmail.com>
2026-09-08 15:34:08 +0300

    feat: add user model and pub mod user

---
+++ src/models/user.rs
@@ -0,0 +1,27 @@
+use serde::(Deserialize, Serialize);
+use sqlx::FromRow;
+use uuid::Uuid;
+
+#[derive(Debug, FromRow, Serialize)]
+pub struct User {
+    pub id: Uuid,
+    pub email: String,
+    #[serde(skip_serializing)]
+    pub password_hash: Option<String>,
+    pub provider: String,
+    pub name: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RegisterRequest {
+    pub email: String,
+    pub password: String,
+    pub name: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+    
