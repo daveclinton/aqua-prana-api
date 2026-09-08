cce50b62dcace0706ef2d7d8dbe94f8abf3bee41
daveclinton <clintondavid46@gmail.com>
2026-09-08 14:38:19 +0300

    add first axum pattern of core files & app state

---
+++ src/state.rs
@@ -0,0 +1,7 @@
+#[derive(Clone)]
+pub struct AppState(
+    pub db: PgPool,
+    pub redis: redis::Client,
+    pub jwt_secret: String,
+    pub ai_client: async_openai::Client<async_openai::config::OpenAIConfig>
+)
